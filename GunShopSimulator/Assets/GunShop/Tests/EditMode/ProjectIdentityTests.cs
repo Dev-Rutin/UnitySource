1fa8ad5b630cdec503c8a732311d6d3c6c0f0cbc
Dev.Rutin <Dev.Rutin@gmail.com>
2026-08-14 20:04:33 +0900

    fix: normalize remaining project identifiers (#11)

--- GunShopSimulator/Assets/GunShop/Tests/EditMode/ProjectIdentityTests.cs
+++ GunShopSimulator/Assets/GunShop/Tests/EditMode/ProjectIdentityTests.cs
@@ -1,3 +1,4 @@
+using System.IO;
 using NUnit.Framework;
 
 namespace Rutin.GunShop.Tests.EditMode
@@ -8,6 +9,14 @@ namespace Rutin.GunShop.Tests.EditMode
         public void ProductName_MatchesUnityProjectName()
         {
             Assert.That(ProjectIdentity.ProductName, Is.EqualTo("GunShopSimulator"));
+        }
+
+        [Test]
+        public void ProjectSettings_DoNotContainLegacyMisspelledName()
+        {
+            var settings = File.ReadAllText("ProjectSettings/ProjectSettings.asset");
+
+            Assert.That(settings, Does.Not.Contain("GunShopSimulatior"));
         }
     }
 }
